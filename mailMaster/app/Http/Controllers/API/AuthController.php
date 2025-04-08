85dd0a44e3b75e1d8b79e5662fdd97fc8b885e3c
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-04-08 11:27:21 +0100

    create the register function

--- mailMaster/app/Http/Controllers/API/AuthController.php
+++ mailMaster/app/Http/Controllers/API/AuthController.php
@@ -3,9 +3,30 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
 }

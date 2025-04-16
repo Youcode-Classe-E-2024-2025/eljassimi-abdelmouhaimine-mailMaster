e61143b05929ec5b451efe9558bf43fc540b12a4
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-04-16 10:23:21 +0100

    add routes for the subscribercontroller

--- mailMaster/routes/api.php
+++ mailMaster/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\NewsletterController;
+use App\Http\Controllers\API\SubscriberController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,6 +10,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
     Route::apiResource('newsletters', NewsletterController::class);
+    Route::apiResource('subscribers', SubscriberController::class);
 
 });
 

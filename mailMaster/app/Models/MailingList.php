b92fa80091786238909bba8eea9c946c6f388422
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-04-16 10:28:05 +0100

    add swagger code to the mailinglist model

--- mailMaster/app/Models/MailingList.php
+++ mailMaster/app/Models/MailingList.php
@@ -8,6 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class MailingList extends Model
 {
     use HasFactory;
+    /**
+     * @OA\Schema(
+     *     schema="MailingList",
+     *     type="object",
+     *     required={"id", "name"},
+     *     @OA\Property(property="id", type="integer", example=1),
+     *     @OA\Property(property="name", type="string", example="Promotions Subscribers"),
+     *     @OA\Property(property="created_at", type="string", format="date-time", example="2025-04-16T12:34:56Z"),
+     *     @OA\Property(property="updated_at", type="string", format="date-time", example="2025-04-16T12:34:56Z")
+     * )
+     */
 
     protected $fillable = ['name', 'description'];
 
